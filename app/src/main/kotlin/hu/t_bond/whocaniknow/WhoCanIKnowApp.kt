a1f21f51842820ac827f480b6e0726c9b60711e9
Tamás Nagy (T-bond) <tbondvagyok@gmail.com>
2022-04-10 01:16:37 +0200

    Use Material You (3) theme

--- app/src/main/kotlin/hu/t_bond/whocaniknow/WhoCanIKnowApp.kt
+++ app/src/main/kotlin/hu/t_bond/whocaniknow/WhoCanIKnowApp.kt
@@ -1,7 +1,14 @@
 package hu.t_bond.whocaniknow
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WhoCanIKnowApp : Application()
+class WhoCanIKnowApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
