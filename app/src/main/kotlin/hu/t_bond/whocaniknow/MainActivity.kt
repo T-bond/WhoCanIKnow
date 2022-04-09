6c420f3f67eb99891792a35ce2670980b64cfc19
Tamás Nagy (T-bond) <tbondvagyok@gmail.com>
2022-04-09 23:57:31 +0200

    Initialize project

---
+++ app/src/main/kotlin/hu/t_bond/whocaniknow/MainActivity.kt
@@ -0,0 +1,18 @@
+package hu.t_bond.whocaniknow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hu.t_bond.whocaniknow.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/kotlin/hu/t_bond/whocaniknow/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package hu.t_bond.whocaniknow.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
