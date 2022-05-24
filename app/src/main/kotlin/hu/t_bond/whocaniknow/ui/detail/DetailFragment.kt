fbb46211ad64f137a1b69fb9d01d50e1449873cd
Tamás Nagy (T-bond) <tbondvagyok@gmail.com>
2022-05-24 16:33:03 +0200

    Add Analytics for detail fragment opening

--- app/src/main/kotlin/hu/t_bond/whocaniknow/ui/detail/DetailFragment.kt
+++ app/src/main/kotlin/hu/t_bond/whocaniknow/ui/detail/DetailFragment.kt
@@ -8,17 +8,22 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.AndroidEntryPoint
 import hu.t_bond.whocaniknow.databinding.DetailFragmentBinding
+
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private lateinit var binding: DetailFragmentBinding
     private val viewModel: DetailViewModel by viewModels()
+    private var firebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(requireContext())
+
         postponeEnterTransition()
         sharedElementEnterTransition =
             TransitionInflater.from(context).inflateTransition(android.R.transition.move)
@@ -34,6 +39,12 @@ class DetailFragment : Fragment() {
         viewModel.contact.observe(viewLifecycleOwner) {
             binding.contact = it
             startPostponedEnterTransition()
+
+            if (it != null) {
+                val bundle = Bundle()
+                bundle.putString(FirebaseAnalytics.Param.ITEM_ID, it.id.value)
+                firebaseAnalytics?.logEvent(FirebaseAnalytics.Event.VIEW_ITEM, bundle)
+            }
         }
 
         val act = activity
